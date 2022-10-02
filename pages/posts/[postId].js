28670042ca02ef86c2ebf923dbac8cc7e10a1933
mahroofalip <99940016+mahroofalip@users.noreply.github.com>
2022-10-02 19:18:07 +0530

    dynamich path setup before we done setup throgh hard code

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -8,24 +8,35 @@ export default function PostDetils({ post }) {
   );
 }
 export async function getStaticPaths() {
+  const response = await (
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+  ).json()
+  const paths  =  response.map(post=>{
+    return {
+      params:{
+        postId: `${post.id}`
+      }
+    }
+  })
   return {
-    paths: [
-      {
-        params: { postId: "1" },
-      },
-      {
-        params: { postId: "2" },
-      },
-      {
-        params: { postId: "3" },
-      },
-      {
-        params: { postId: "4" },
-      },
-      {
-        params: { postId: "5" },
-      },
-    ],
+    // paths: [
+    //   {
+    //     params: { postId: "1" },
+    //   },
+    //   {
+    //     params: { postId: "2" },
+    //   },
+    //   {
+    //     params: { postId: "3" },
+    //   },
+    //   {
+    //     params: { postId: "4" },
+    //   },
+    //   {
+    //     params: { postId: "5" },
+    //   },
+    // ],
+    paths,
     fallback:false,
   };
 }
